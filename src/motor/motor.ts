c4d43356f149e6eb619e246696fffe1de6d2dde9
xxxCoeNxxx <pedrocela1992@gmail.com>
2025-09-22 15:11:20 +0200

    datos del ejercicio

---
+++ src/motor/motor.ts
@@ -0,0 +1,13 @@
+import { LineaTicket } from "../modelo/model.js";
+
+const calculaTicket = (lineasTicket: LineaTicket[]) => {
+  for (let i = 0; i < lineasTicket.length; i++) {
+    // ...
+  }
+};
+
+/* const calculaTicket = (lineasTicket: LineaTicket[]) => {
+  lineasTicket.reduce
+    // ...
+    ();
+}; */
